3a1247dd64f5234721992b3a9491cc00de8e7dbf
Sia Cheong Hui <brendan.bnsch@paydaes.com>
2024-03-14 09:57:50 +0800

    test changes

--- src/main/java/com/demoproject/brendansia/service/DemoService.java
+++ src/main/java/com/demoproject/brendansia/service/DemoService.java
@@ -50,6 +50,8 @@ public class DemoService {
     }
 
     public String processProduct(SaveRequestDTO requestDTO, String code){
+        //test new changes
+        Products newProduct = new Products();
         Products existingProduct = productsRepository.getByCode(code);
         if (Objects.isNull(existingProduct)) {
             return "Product does not exist";
