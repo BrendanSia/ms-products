5537d97f7e8b0f8d93efac8b750299260e03d855
Sia Cheong Hui <brendan.bnsch@paydaes.com>
2024-07-24 09:26:33 +0800

    add validation

--- src/main/java/com/demoproject/brendansia/dto/SaveRequestDTO.java
+++ src/main/java/com/demoproject/brendansia/dto/SaveRequestDTO.java
@@ -1,5 +1,7 @@
 package com.demoproject.brendansia.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SaveRequestDTO {
     private Integer id;
+
+    @NotBlank(message = "Code is required")
     private String code;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name length must be between 2 and 50")
     private String name;
     private String category;
     private String brand;
