5061cfaa94128ed5e5ce17fb86bdb088deb144c2
Sia Cheong Hui <brendan.bnsch@paydaes.com>
2024-08-20 15:08:50 +0800

    remove unused methods

--- src/main/java/com/demoproject/brendansia/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/demoproject/brendansia/exceptions/GlobalExceptionHandler.java
@@ -2,16 +2,12 @@ package com.demoproject.brendansia.exceptions;
 
 import com.demoproject.brendansia.constant.Error;
 import com.demoproject.brendansia.dto.ErrorResponseDTO;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ResponseBody
@@ -27,12 +23,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(failStatus, error.getCode(), error.getDescription());
         return ResponseEntity.status(HttpStatus.OK).body(errorResponse);
     }
-
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-//            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-//        return new ResponseEntity<Object>(ex.getFieldError().getDefaultMessage(), HttpStatusCode);
-//    }
-
-
 }
